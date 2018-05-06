85d06cafbdfe15ea0a0f1eae64bf6b7968a6d008
Luciano Sartor <luciano.sartor@ing.austral.edu.ar>
2018-05-06 13:24:38 +0200

    Added email verification in register

--- javascript/register.js
+++ javascript/register.js
@@ -1,56 +1,60 @@
-$( document ).ready(function() {
-  $('#firstnameError').hide();
-  $('#lastnameError').hide();
-  $('#emailError').hide();
-  $('#passwordError').hide();
+$(document).ready(function () {
+    $('#firstnameError').hide();
+    $('#lastnameError').hide();
+    $('#emailError').hide();
+    $('#passwordError').hide();
 });
 
 function registerUser() {
-  resetErrors();
-  if (checkRegistrationForm()) {
-    // todo: uncomment post
-
-    $.post("php/register.php",
-    {
-      name: $("#firstname").val(),
-      lastname: $("#lastname").val(),
-      email: $("#email").val(),
-      password: sha256($("#password").val())
-    }, function(result) {
-      console.log(result);
-      //window.location.href = 'index.html';
-    });
-  }
+    resetErrors();
+    if (checkRegistrationForm()) {
+        $.post("php/register.php",
+            {
+                name: $("#firstname").val(),
+                lastname: $("#lastname").val(),
+                email: $("#email").val(),
+                password: sha256($("#password").val())
+            }, function (result) {
+                var response = JSON.parse(result);
+                if (response.status !== 200) {
+                    var error = $('#responseError');
+                    error.html(response.msg);
+                    error.show();
+                } else {
+                    window.location.href = 'index.html';
+                }
+            });
+    }
 }
 
 function checkRegistrationForm() {
-  var firstname = $('#firstname').val();
-  var lastname = $('#lastname').val();
-  var email = $('#email').val();
-  var password = $('#password').val();
+    var firstname = $('#firstname').val();
+    var lastname = $('#lastname').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
 
-  var errors = 0;
-  if (firstname == undefined || firstname.length == 0) {
-    errors++;
-    $('#firstname').addClass('is-invalid');
-    $('#firstnameError').show();
-  }
-  if (lastname == undefined || lastname.length == 0) {
-    errors++;
-    $('#lastname').addClass('is-invalid');
-    $('#lastnameError').show();
-  }
-  if (email == undefined || email.length == 0 || !validateEmail(email)) {
-    errors++;
-    $('#email').addClass('is-invalid');
-    $('#emailError').show();
-  }
-  if (password == undefined || password.length < 6) {
-    errors++;
-    $('#password').addClass('is-invalid');
-    $('#passwordError').show();
-  }
-  return errors == 0;
+    var errors = 0;
+    if (firstname == undefined || firstname.length == 0) {
+        errors++;
+        $('#firstname').addClass('is-invalid');
+        $('#firstnameError').show();
+    }
+    if (lastname == undefined || lastname.length == 0) {
+        errors++;
+        $('#lastname').addClass('is-invalid');
+        $('#lastnameError').show();
+    }
+    if (email == undefined || email.length == 0 || !validateEmail(email)) {
+        errors++;
+        $('#email').addClass('is-invalid');
+        $('#emailError').show();
+    }
+    if (password == undefined || password.length < 6) {
+        errors++;
+        $('#password').addClass('is-invalid');
+        $('#passwordError').show();
+    }
+    return errors == 0;
 }
 
 function validateEmail(email) {
@@ -59,12 +63,13 @@ function validateEmail(email) {
 }
 
 function resetErrors() {
-  $('#firstname').removeClass('is-invalid');
-  $('#firstnameError').hide();
-  $('#lastname').removeClass('is-invalid');
-  $('#lastnameError').hide();
-  $('#email').removeClass('is-invalid');
-  $('#emailError').hide();
-  $('#password').removeClass('is-invalid');
-  $('#passwordError').hide();
+    $('#firstname').removeClass('is-invalid');
+    $('#firstnameError').hide();
+    $('#lastname').removeClass('is-invalid');
+    $('#lastnameError').hide();
+    $('#email').removeClass('is-invalid');
+    $('#emailError').hide();
+    $('#password').removeClass('is-invalid');
+    $('#passwordError').hide();
+    $('#responseError').hide();
 }
--- php/register.php
+++ php/register.php
@@ -2,10 +2,13 @@
 include 'db.php';
 include 'sendEmail.php';
 
-
+$response = array('status' => 0, 'msg' => "");
 if (isset($_POST["name"]) && isset($_POST["lastname"]) && isset($_POST["email"]) && isset($_POST["password"])) {
     if (!checkConnection()) {
-        echo "Db connection error: \n" . getError();
+        $response['status'] = 500;
+        $response['msg'] = "Server error occurred. Please try again";
+        $response['error'] = "" . getError();
+        echo json_encode($response);
         exit();
     } else {
         $name = $_POST["name"];
@@ -14,18 +17,28 @@ if (isset($_POST["name"]) && isset($_POST["lastname"]) && isset($_POST["email"])
         $password = $_POST["password"];
         $id = 0;
 
-        $answer = query("SELECT max(id) as maximum FROM user;");
-        while ($ansObj = $answer->fetch_object()) {
-            $id = $ansObj->maximum;
-            $id++;
+        $checkEmail = query("SELECT count(*) as total from user where email='$email'");
+        if (($checkEmail->fetch_object())->total != 0) {
+            $response['status'] = 400;
+            $response['msg'] = "Mail already registered";
+            $response['error'] = "$email already exists in database.";
+            echo json_encode($response);
+            exit();
+        } else {
+            $answer = query("SELECT max(id) AS maximum FROM user;");
+            while ($ansObj = $answer->fetch_object()) {
+                $id = $ansObj->maximum;
+                $id++;
+            }
+            $answer->close();
+
+            $result = query("INSERT INTO user (id, name, lastname, email, password) VALUE ('$id', '$name', '$lastname', '$email', '$password');");
+            closeConnection();
+            $to = welcomeMail($email);
+            $response['status'] = 200;
+            $response['msg'] = "Mail sent to: $to";
+            echo json_encode($response);
         }
-        $answer->close();
-
-        $result = query("INSERT INTO user (id, name, lastname, email, password) VALUE ('$id', '$name', '$lastname', '$email', '$password');");
-        closeConnection();
-        $to = welcomeMail($email);
-        echo "success: sent mail \n" . $to;
-
     }
 } else {
     echo "invalid data";
