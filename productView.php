d22cc4a7e2e97c80960e1763b202bd17a980cb86
Luciano Sartor <luciano.sartor@ing.austral.edu.ar>
2018-05-06 19:14:33 +0200

    Working on nav

--- productView.php
+++ productView.php
@@ -24,7 +24,7 @@ if ($id && isConnected()) {
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="flex-grow-1">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="index.html">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
